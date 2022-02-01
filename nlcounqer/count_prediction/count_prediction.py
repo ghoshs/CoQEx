dd2563eadfefe0a2f04df7c05cdd9b4e1ebff496
ghoshs <ghoshs@mpi-inf.mpg.de>
2022-02-01 20:11:58 +0100

    time elapsed in different tasks

--- nlcounqer/count_prediction/count_prediction.py
+++ nlcounqer/count_prediction/count_prediction.py
@@ -4,6 +4,7 @@ from count_prediction.count_extraction import get_cogcomp_ntuples, get_count_spa
 from count_prediction.apply_aggregator import apply_aggregator
 import os
 from os import path
+import time
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 def get_noun_phrase_w_count(nlp, context, answer, start):
@@ -15,14 +16,20 @@ def get_noun_phrase_w_count(nlp, context, answer, start):
 
 
 def predict_count(query, contexts, tfmodel, thresholds, aggregator, nlp):
+	time_elapsed_prediction = 0
+	time_elapsed_extraction = 0
+	time_elapsed_aggregation = 0
 	for item in contexts:
 		## 1. span prediction 
 		try:
+			tic = time.perf_counter()
 			answer = tfmodel(question=query, context=item['context'])
+			time_elapsed_prediction += time.perf_counter() - tic
 		except:
 			answer = {'answer':'', 'score': 0, 'start': -1}
 		finally:
 			##2. Count extraction
+			tic = time.perf_counter()
 			item['count_span'] = {'text': answer['answer'], 'score': answer['score'], 'start': answer['start']}
 			np_start, np_end = answer['start'], answer['start']+len(answer['answer'])
 			# np_start, np_end = get_noun_phrase_w_count(nlp, item['context'], answer['answer'], answer['start'])
@@ -39,7 +46,11 @@ def predict_count(query, contexts, tfmodel, thresholds, aggregator, nlp):
 				cardinal = float(count_span[0]['quantity']) if len(count_span)>0 else None
 			finally:
 				item['cardinal'] = int(cardinal) if cardinal is not None and int(cardinal) > 0 else None 
+			time_elapsed_extraction += time.perf_counter() - tic
 
 	##3. Evaluate
+	tic = time.perf_counter()
 	prediction, sorted_data, annotated_contexts = apply_aggregator(contexts, aggregator, thresholds)
+	time_elapsed_aggregation += time.perf_counter() - tic
+	print('Prediction took %.4f secs\nExtraction took %.4f secs\nAggregation took %.4f secs'%(time_elapsed_prediction, time_elapsed_extraction, time_elapsed_aggregation))
 	return prediction, sorted_data, annotated_contexts
