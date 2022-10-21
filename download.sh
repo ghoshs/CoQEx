831dbfe334f35e46598352b7355383350124e97b
ghoshs <5251447+ghoshs@users.noreply.github.com>
2022-10-22 00:11:31 +0200

    update dataset desc

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ echo 'root dir:' $COQEX_ROOT
 echo "Creating a dataset dir if necessary .."
 mkdir -p ${COQEX_ROOT}/dataset/
 
-echo "Downloading CoQuAD_v1 data .."
+echo "Downloading CoQuAD train and evaluation data .."
 wget -r -O coquad.zip https://nextcloud.mpi-klsb.mpg.de/index.php/s/kiWC5wEgR9nxws5/download
 unzip coquad.zip -d ${COQEX_ROOT}/dataset/
 rm coquad.zip
@@ -23,10 +23,4 @@ echo "Downloading count query Stresstest .."
 wget -r -O stresstest.zip https://nextcloud.mpi-klsb.mpg.de/index.php/s/G5NfyMo4DARJGAF/download
 unzip stresstest.zip -d ${COQEX_ROOT}/dataset/
 rm stresstest.zip
-echo "Download complete!"
-
-echo "Downloading CoQuAD_v2 dataset: train, test, dev.."
-wget -r -O stresstest.zip https://nextcloud.mpi-klsb.mpg.de/index.php/s/Qcno2Ta7qgAQ7cZ/download
-unzip coquad_v2.zip -d ${COQEX_ROOT}/dataset/
-rm coquad_v2.zip
 echo "Download complete!"
